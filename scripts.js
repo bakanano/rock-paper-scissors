4375e7133d765d26f94bad1258bea9d46bbe130b
bakanano <narutoz0rs@live.com>
2021-11-23 15:37:08 +1300

    Created playRound function

--- scripts.js
+++ scripts.js
@@ -2,3 +2,26 @@ function computerPlay() {
     let items = ["Rock", "Paper", "Scissors"];
     return items[Math.floor(Math.random() * items.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    let result = decideWinner(playerSelection, computerSelection);
+    console.log(result);
+    return showResult(result, playerSelection, computerSelection);
+}
+
+let roundsPlayedDiv = document.querySelector(".round-count");
+let roundScoreDiv = document.querySelector(".round-score");
+let roundOutcomeDiv = document.querySelector(".round-outcome");
+let finalOutcome = document.querySelector(".final-results");
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => { 
+    button.addEventListener("click", (e) => {
+        let playerSelection = e.target.textContent;
+        let computerSelection = computerPlay();
+        let roundResult = playRound(playerSelection, computerSelection);
+        roundsPlayed++;
+    })
+})
+
+
